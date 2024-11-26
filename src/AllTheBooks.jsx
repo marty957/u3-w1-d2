df01e9ca97545ce99f5f546e49ef91a878732fda
marty957 <fantuzzimartina@yahoo.it>
2024-11-26 16:09:29 +0100

    second commit

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -3,9 +3,29 @@ import { Col, Container, Row, Button, Card, Badge } from "react-bootstrap";
 import fantasy from "./data/fantasy.json";
 
 class AllTheBooks extends Component {
+  state = {
+    selectBooks: null
+  };
   render() {
     return (
       <Container fluid="md" className="mt-2">
+        <Container fluid="md" className="my-2 ">
+          <Button variant="info" className="mx-4">
+            Fantasy
+          </Button>
+          <Button variant="info" className="mx-4">
+            History
+          </Button>
+          <Button variant="info" className="mx-4">
+            Horror
+          </Button>
+          <Button variant="info" className="mx-4">
+            Romance
+          </Button>
+          <Button variant="info" className="mx-4">
+            Scifi
+          </Button>
+        </Container>
         <Row>
           {fantasy.map((book) => (
             <Col xs={6} md={4} xl={3} key={`card-${book.asin}`}>
